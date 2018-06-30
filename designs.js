96f8e50467c9b62eb146832a15abc3acefddf2de
yusuf-abdoul <40715160+yusuf-abdoul@users.noreply.github.com>
2018-06-30 23:32:47 +0100

    updated

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
-// Select color input
-// Select size input
+//select color input
+function addColor(){
+	let col = $("td");
+	col.click(function(){
+		const color = $("#colorPicker").val();
+		if ($(this).attr("style")){
+			$(this).removeAttr("style");
+		} 
+		else{
+			$(this).attr("style","background-color:"+color);
+		} 
+	}	
+);
+}
+//Select size input
+$("#sizePicker").submit(function(evt){
+	evt.preventDefault();
+	let height = $("#inputHeight").val();
+	let weight = $("#inputWeight").val();
+	makeGrid(height, weight);
+	addColor();
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+}
+);
+function makeGrid(row, col) {
+	$("tr").remove();
+	let  i=1;
+	while(i <=row){
+		$("#pixelCanvas").append("<tr></tr>");
+		i++;
+	}
+	let j=1;
+	while(j<= col){
+		$("tr").append("<td></td>");
+		//$("tr").filter(':last').append("<td></td>");
+		j++;
+	}
 }
